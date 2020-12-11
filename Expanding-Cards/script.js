00786e0857c7900b76104fc4f63620b85d596fea
Gangavarapu <mgangavarapu@in.imshealth.com>
2020-12-11 11:03:53 +0530

    Developing Expanding cards

---
+++ Expanding-Cards/script.js
@@ -0,0 +1,15 @@
+// Get Panels
+
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    // Remove all active classes
+    removeActiveClass();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClass() {
+  panels.forEach((panel) => panel.classList.remove("active"));
+}
